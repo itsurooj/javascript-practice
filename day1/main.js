c6f9dbf5eab958ca964479941aa71796523c5e0e
itsurooj <fatimabatool1150@gmail.com>
2021-07-15 15:21:34 -0700

    adding first task

---
+++ day1/main.js
@@ -0,0 +1,16 @@
+
+    console.log("hello world");
+    let a = 2;
+    const b =3;
+
+    let c = a+b;
+    console.log (c);
+
+    let string ="hellow world";
+
+    let d =1.0999;
+    console.log(string);
+    console.log (d);
+    let e = string+d;
+
+    console.log(e);
