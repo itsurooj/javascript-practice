3729dcf3b540cc9179cf2d16bbe0755727c6c472
itsurooj <fatimabatool1150@gmail.com>
2021-07-15 16:51:51 -0700

    adding second assignment

--- day1/main.js
+++ day1/main.js
@@ -11,6 +11,17 @@
     let d =1.0999;
     console.log(string);
     console.log (d);
-    let e = string+d;
+    let e = b*d;
 
     console.log(e);
+
+    let team= ["razia", "naina", "ali"];
+    c//onsole.log(team[2]);
+
+    for(i=0; i<=2; i++){
+
+        console.log(team[i]);
+    }
+    for(i=2; i>=0; i--){
+        console.log(team[i]);
+    }
