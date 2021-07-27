21ce5bc9900f3636336fe25ea1307392a291cd01
itsurooj <fatimabatool1150@gmail.com>
2021-07-27 16:33:58 -0700

    string Methods

---
+++ string-method.js
@@ -0,0 +1,93 @@
+
+// Lenght is a function return string length
+
+let str = "javascript is  great language";
+
+let a = str.length;
+
+
+console.log(a);
+
+// lowercase and uppercase
+
+
+
+let str1 = "Javascript is  Great Language";
+
+
+let b = str1.toLowerCase();
+
+
+console.log(b +"   convert into lower case");
+
+
+
+// Uppercase
+
+let str2 = "javascript is great language";
+
+
+let c = str1.toUpperCase();
+
+
+console.log(c +"   convert into upper case");
+
+
+
+// Includes function it will return true or false
+
+
+let str3 = "javascript is great language";
+
+let d = str3.includes("great");
+
+console.log(d);
+
+
+// Search method it will return the position  no of word 
+
+let str4 = "javascript is great language";
+
+let e = str4.search("great");
+
+console.log(e);
+
+// Match function
+
+let str5 = "javascript is great  is is language";
+
+let f = str5.match(/is/g);
+
+
+console.log(f);
+
+
+
+
+// Indexof function
+
+let str6 = "javascript is great  is is language";
+
+let g = str6.indexOf("is");
+
+
+console.log(g);
+
+// Last indexOf function
+
+
+let str7 = "javascript is great  is is language";
+
+let h = str7.lastIndexOf("is");
+
+
+console.log(h);
+
+
+
+
+
+
+
+
+
