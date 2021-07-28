89c688888beb4d3c0b22c28bda00b2cf2af34cdf
itsurooj <fatimabatool1150@gmail.com>
2021-07-28 13:24:31 -0700

    Template Strings

---
+++ templets-string.js
@@ -0,0 +1,18 @@
+
+// Templetes Strings 
+
+// Concatination
+
+var user = "Urooj Fatima";
+
+var b = 2021;
+
+
+var a = ` Hello ${user} <br> `;
+
+document.write(a);
+console.log(a);
+
+document.write(`  ${a} Wellcome to ${b}`);
+
+console.log(`  ${a} Wellcome to ${b} `);
